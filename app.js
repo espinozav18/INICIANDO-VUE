00b84de604422bd972cc8393bdfc41c83c396c10
espinozav18 <exonespinoza@gmail.com>
2021-11-10 19:53:19 -0500

    desafio2

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello ' ,
-      city: 'Lima',
-      country: 'Peru',
-      name: 'Espinoza',
-      numero1:20,
-      numero2:30
+      mensaje:"Hola soy de Perú"
+      
     },
     methods:{
-        imprimirEnConsola(){
-            console.log("Hola mundo");
-        }
-    },
-    computed:{
-        alert(){
-            alert(this.message);
+        mensaje2(){
+            alert(this.mensaje);
         }
     }
 
